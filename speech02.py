d8ca7f5bf925a8ba5967c5888b379732f53a93ba
Bryan <tw_bryan_chen@intretech.com>
2019-03-21 10:32:21 +0800

    Create speech02.py

---
+++ speech02.py
@@ -0,0 +1,13 @@
+import time
+from pygame import mixer
+mixer.init()
+mixer.music.load('hello.mp3')
+mixer.music.play()
+time.sleep(2)
+mixer.music.load('hello2.mp3')
+mixer.music.play()
+time.sleep(4)
+mixer.music.load('hello3.mp3')
+mixer.music.play()
+time.sleep(4)
+
