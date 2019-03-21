3c62e8fdab2c4c40fab1e1e022fcbf9eca2f5e93
Bryan <tw_bryan_chen@intretech.com>
2019-03-21 09:40:59 +0800

    add and update

--- speech01.py
+++ speech01.py
@@ -1,3 +1,7 @@
 from gtts import gTTS
 tts = gTTS(text='hello', lang='en')
 tts.save("hello.mp3")
+tts = gTTS(text='恭喜發財,紅包拿來', lang='zh-tw')
+tts.save("hello2.mp3")
+tts = gTTS(text='恭喜發財,紅包拿來', lang='zh-cn')
+tts.save("hello3.mp3")
